396a8755c1e9a6ba01017b1643a1aacd1e86d1d0
ashutoshaks <ashu11.03.2002@gmail.com>
2023-04-02 19:25:43 +0530

    Final update

--- Group_4_HPCA_Assignment_1/simulate-top-10.py
+++ Group_4_HPCA_Assignment_1/simulate-top-10.py
@@ -15,50 +15,44 @@ bp_type = ['TournamentBP', 'BiModeBP']
 numROBEntries = [128, 192]
 numIQEntries = [16, 64]
 
-# Create a list of all possible combinations of parameters
-params = [LQEntries, SQEntries, l1d_size, l1i_size,
-          l2_size, bp_type, numROBEntries, numIQEntries]
-param_combinations = list(itertools.product(*params))
+param_combinations = [
+    (32, 64, '64kB', '16kB', '512kB', 'TournamentBP', 192, 64),
+    (32, 64, '64kB', '16kB', '256kB', 'TournamentBP', 192, 64),
+    (32, 64, '32kB', '16kB', '256kB', 'TournamentBP', 192, 64),
+    (64, 64, '64kB', '16kB', '512kB', 'TournamentBP', 192, 64),
+    (64, 64, '32kB', '16kB', '512kB', 'TournamentBP', 192, 64),
+    (32, 64, '32kB', '16kB', '512kB', 'TournamentBP', 192, 64),
+    (64, 64, '64kB', '16kB', '256kB', 'TournamentBP', 192, 64),
+    (64, 64, '32kB', '16kB', '256kB', 'TournamentBP', 192, 64),
+    (32, 64, '64kB', '16kB', '512kB', 'TournamentBP', 128, 64),
+    (32, 64, '64kB', '16kB', '256kB', 'TournamentBP', 128, 64)
+]
 param_titles = ['LQEntries', 'SQEntries', 'l1d_size', 'l1i_size',
                 'l2_size', 'bp_type', 'numROBEntries', 'numIQEntries']
 
 parser = argparse.ArgumentParser(
-    description='Simulating all 256 configurations.')
+    description='Simulating the top 10 configurations.')
 parser.add_argument('-g', type=str, default='~/gem5',
                     help='Path to the gem5 directory.')
+parser.add_argument('--rank', type=int, default=-1,
+                    help='Rank of the configuration to simulate.')
 options = parser.parse_args()
 gem5_dir = os.path.expanduser(options.g)
 
 # Create a directory to store the results
-result_dir = os.path.join(gem5_dir, 'configs/assignment/results')
+result_dir = os.path.join(gem5_dir, 'configs/assignment/top-10-results')
 if not os.path.exists(result_dir):
     os.makedirs(result_dir)
 
-# The log file helps to identify which simulations have been completed
-log_file = os.path.join(gem5_dir, 'configs/assignment/simulate.log')
-if not os.path.exists(log_file):
-    open(log_file, 'w').close()
-
-# Store the completed simulations in a set
-completed_params = set()
-with open(log_file, 'r') as f:
-    for line in f:
-        completed_params.add(line.strip())
-
-
-logging.basicConfig(filename=log_file, format='%(message)s',
-                    level=logging.INFO, filemode='a')
-logger = logging.getLogger()
-
 # Run simulations with all possible combinations of parameters
-counter = 1
-for param in param_combinations:
-    # Create a directory to store the results for this combination of parameters
-    outdir = '-'.join(str(x) for x in param)
-    if outdir in completed_params:
+for i, param in enumerate(param_combinations):
+    if options.rank != -1 and options.rank != i + 1:
         continue
 
-    print(f'Running simulation {counter} with parameters:', flush=True)
+    # Create a directory to store the results for this combination of parameters
+    outdir = f'rank-{(i + 1):02d}-' + ('-'.join(str(x) for x in param))
+
+    print('Running simulation with parameters:', flush=True)
     print(', '.join(f'{x}: {y}' for x, y in zip(
         param_titles, param)), flush=True)
     print()
@@ -79,8 +73,3 @@ for param in param_combinations:
     ret = subprocess.run(cmd, shell=True, check=True)
     print(
         f'Simulation completed with return code {ret.returncode}', flush=True)
-
-    # Log the completed simulation
-    if ret.returncode == 0:
-        logger.info(outdir)
-        counter += 1
